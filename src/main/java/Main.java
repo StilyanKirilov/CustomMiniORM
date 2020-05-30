6cd14562d1f603511d71c0df48ca828f37d65f1a
StilyanKirilov <60314753+StilyanKirilov@users.noreply.github.com>
2020-05-30 14:54:52 +0300

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import entities.User;
+import orm.Connector;
+import orm.EntityManager;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, IllegalAccessException, NoSuchMethodException, InstantiationException, InvocationTargetException {
+
+        String username = "root";
+        String password = "toronto1995";
+
+        Connector.createConnection(username, password, "orm_db");
+        EntityManager<User> entityManager = new EntityManager<>(Connector.getConnection());
+
+//        User user = new User("Sasho", "12345", 21, new Date());
+        User user = entityManager.findFirst(User.class," id = 3 ");
+        entityManager.delete(user);
+        System.out.println("User 3 is deleted");
+//        entityManager.persist(user2);
+//        List<User> users = (List<User>) entityManager.find(User.class, " age > 23");
+//
+//        for (User userr : users) {
+//            System.out.println(userr.getUsername());
+//        }
+//        System.out.println("--------------");
+//        List<User> allUsers = (List<User>) entityManager.find(User.class);
+//        for (User userr : allUsers) {
+//            System.out.println(userr.getUsername());
+//        }
+    }
+}
